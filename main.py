4455b2c2eb78e107d2ea2ca668f8eaa15286b133
Dawn <t.diepenbroek@live.com>
2024-12-28 18:01:00 +0200

    added git ignore, completed multiple shapes from the same origin project

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+from random import random
 
 timmy = Turtle()
 timmy.shape("turtle")
@@ -17,13 +18,19 @@ timmy.color("forest green")
     timmy.forward(10)"""
 
 #drawing multiple shapes from the same origin point
-sides = [3, 4, 5, 6, 7, 8, 9, 10]
+"""sides = [3, 4, 5, 6, 7, 8, 9, 10]
 timmy.pendown()
 for shapes in sides:
-    angles = 360/shapes
+    angles = 180 - ((shapes-2)*180)/shapes
+    r = random()
+    g = random()
+    b = random()
+    timmy.pencolor((r,g,b))
     for draw in range(shapes):
         timmy.rt(angles)
-        timmy.forward(100)
+        timmy.forward(100)"""
+
+#
 
 
 screen = Screen()
